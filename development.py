aa82c11d2560d5907647422ba1f77d451eeabdfc
MykolaBordakov <m.m.bordakov@gmail.com>
2022-06-16 20:40:08 +0300

    New files in to development

---
+++ development.py
@@ -0,0 +1,8 @@
+print('Commad: {} hase output:  {}'.format( 'echo  `uname -a`', 
+                        'MINGW64_NT-10.0-22000 DESKTOP-U70F4SV 3.3.4-341.x86_64 2022-05-09 11:56 UTC x86_64 Msys'))
+
+print('Commad: {} hase output:  {}'.format( 'echo `ps -ef`', 
+                        'UID PID PPID TTY STIME COMMAND mmbor 1276 1275 pty0 19:46:17 /usr/bin/bash mmbor 1275 1 ? 19:46:17 /usr/bin/mintty mmbor 1370 1276 pty0 20:11:35 /usr/bin/ps'))
+
+print('Commad: {} hase output:  {}'.format( 'echo ` df -h`', 
+                        'Filesystem Size Used Avail Use% Mounted on C:/Program Files/Git 242G 114G 128G 47% / D: 235G 25G 211G 11% /d'))
